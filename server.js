12641bd73872e3cc5cd5f18eebde89d3306cb851
Anita Murali <amurali@unc.edu>
2023-04-10 15:55:37 -0400

    edit server.js

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ const port = argv.port || 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
+    fs.readFile('./public/index.html', (err, data) => {
 
 
 
